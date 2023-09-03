46665c3eb1fedff0d54b0b21f6b6fddc90823896
Aishwarya Janakiraman <aishwarya9425@gmail.com>
2023-09-03 18:40:37 +0530

    prop types

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import StarRating from "./StarRating";
 // import "./index.css";
 // import App from "./App";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={10}/>
+    <StarRating maxRating={10} defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
